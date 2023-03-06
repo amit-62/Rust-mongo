77b53cbf21fd8f3853edbba71fed40264149199c
amit-62 <kramit6662@gmail.com>
2023-03-07 01:14:21 +0530

    db connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::prelude::*;
+use db::DB;
+use serde::{Serialize, Deserialize};
+use std::convert::Infallible;
+
+use warp::{Filter, Rejection};
+
+type Result<T>  = std::result::Result<T, error::Error>;
+type WebResult<T> = std::result::Result<T, Rejection>;
+
+mod db;
+mod error;
+mod handler;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Book {
+    pub id: String,
+    pub name: String,
+    pub author: String,
+    pub num_page: usize,
+    pub added_at: DateTime<Utc>,
+    pub tags: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let db = DB::init().await?;
+    let book = warp::path("book");
+
+    let book_routes = book
+    .and(warp::post())
+    .and(warp::body::json())
+    .and(with_db(db.clone()))
+    .and_then(handler::create_book_handler)
+    .or(book
+        .and(warp::put())
+        .and(warp::path::param())
+        .and(warp::body::json())
+        .and(with_db(db.clone()))
+        .and_then(handler::edit_book_handler))
+    .or(book
+        .and(warp::delete())
+        .and(warp::path::param())
+        .and(with_db(db.clone()))
+        .and_then(handler::delete_book_handler))
+    .or(book
+        .and(warp::get())
+        .and(with_db(db.clone()))
+        .and_then(handler::books_list_handler));
+
+    let routes = book_routes.recover(error::handler_rejection);
+    println!("started on port 8080");
+    warp::serve(routes).run(([0,0,0,0], 8080)).await;
+    ok(());    
+
+}
+
+fn with_db(db: DB) -> imp Filter<Extract = (DB,), Error = Infallible> +clone {
+    warp::any().map(move || db.clone())
 }
